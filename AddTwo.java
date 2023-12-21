afadc6b7b9b85c83592bf79cd84662d33b9f13c0
debops123 <devorah.cohen@post.runi.ac.il>
2023-12-21 17:17:05 +0200

    almost done

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,10 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		//  a program that adds two given integers and prints the result in a fancy way
+		 int a = Integer.parseInt(args[0]);
+		 int b = Integer.parseInt(args[1]);
+		 System.out.println(a + " + "+ b + " = " + (a + b));
+
 	}
 }
--- Coins.java
+++ Coins.java
@@ -4,6 +4,9 @@
  */
 public class Coins {
 	public static void main(String[] args) {
-		// Put your code here
+		//  gets a number of cents as a commandline argument and prints how to represent this quantity using as many quarters as possible plus
+// the remainder in cents
+		int num_of_cents = Integer.parseInt(args[0]);
+		System.out.println("Use " + num_of_cents/25 + " quarters and " + num_of_cents%25 + " cents");
 	}
 }
