dd0bb2ebb79c80621f2b3e098549d97c885823c0
debops123 <devorah.cohen@post.runi.ac.il>
2023-12-22 12:00:12 +0200

    final

--- LinearEq.java
+++ LinearEq.java
@@ -5,13 +5,14 @@
  * Treats the three arguments as well as the computed value as double values
  */
 public class LinearEq {
-	// a program that solves linear equations of the form a * x + b = c
-	double a = Double.parseDouble(args[0]);
-	double b = Double.parseDouble(args[1]);
-	double c = Double.parseDouble(args[2]);
-	double x = (c - b) / a ;
-	System.out.println( a + " * x + "+ b + " = "+c);
-	System.out.println( "x = " + x);
-	//System.out.printf("%.1f * x + %.1f = %.1f\n", a, b, c);
-   // System.out.printf("x = %.1f\n", x);
+	public static void main(String[] args) {
+		// a program that solves linear equations of the form a * x + b = c
+		double a = Double.parseDouble(args[0]);
+		double b = Double.parseDouble(args[1]);
+		double c = Double.parseDouble(args[2]);
+		double x = (c - b) / a ;
+		System.out.println( a + " * x + "+ b + " = "+c);
+		System.out.println( "x = " + x);
+
+	}
 }
--- Triangle.java
+++ Triangle.java
@@ -5,6 +5,11 @@
  */ 
 public class Triangle {
 	public static void main(String[] args) {
-		// Put your code here	
+		// a program that tests if three given integers form a triangle
+		int a = Integer.parseInt(args[0]);
+		int b = Integer.parseInt(args[1]);
+		int c = Integer.parseInt(args[2]);
+		
+		System.out.println(a + " , " + b + " , "+ c + ": " + ((a + b) > c));
 	}
 }
